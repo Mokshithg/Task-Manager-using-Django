42b1bc6405a6f80310057e00e168271f6d773596
Mokshith <garipallymokshith@gmail.com>
2024-08-05 23:35:07 +0530

    changes

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from .models import Task
+from django.views.generic import ListView, CreateView, DetailView
+from .models import Task
+
 
 def task_list(request):
     tasks = Task.objects.all()
@@ -8,3 +14,16 @@ def task_list(request):
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'task_detail.html', {'task': task})
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title', 'description', 'due_date', 'completed']
+    template_name = 'task_form.html'
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
+    path('', views.TaskListView.as_view(), name='task_list'),
+    path('task/create/', views.TaskCreateView.as_view(), name='task_create')
 ]
