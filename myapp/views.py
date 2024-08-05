a0ad3cfb933ac3306252b3fe0c4f5d5bb15ea884
Mokshith <garipallymokshith@gmail.com>
2024-08-05 22:39:55 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'task_detail.html', {'task': task})
